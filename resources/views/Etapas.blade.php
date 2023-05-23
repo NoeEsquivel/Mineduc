57e26111d368e13b1a0ae5e414e577ae7ba25f28
NoeEsquivel <eesquivelc@miumg.edu.gt>
2023-05-22 23:12:48 -0600

    Vista Etapas, creada especificamente para instrucciones del proyecto

---
+++ resources/views/Etapas.blade.php
@@ -0,0 +1,35 @@
+{{--Pasos del proyecto
+
+INSTRUCCIONES PARA EL EQUIPO:
+1. Cuando comiencen siempre hagan un pull para descargar avances.
+2. Al finalizar o hacer un avance vayan haciendo su respectivo commit y push, de preferencia haganlo en el programa PHP
+por la opcion git que el mismo te da, de esa manera ya probamos que no da problemas.
+3. Si es posible avisen en el grupo que van a meter mano al proyecto, de esta manera los demas estamos conscientes de que en
+ese preciso momento alguien mas esta trabajando tambien, por si surjen cambios inesperados.
+4. Cada vez que realicen un poco de codigo que consideren escencial, HAGAN UN COMENTARIO DEL MISMO, mientras mas preciso
+y extenso el comentario explicando para que funciona mejor, por que de esta forma a la hora de exponer no nos complicamos mucho.
+5. Al final de esta vista abra uan seccion de novedades, siempre revisenla para ver si algun compañero dejo una novedad,
+de igual forma si ustedes quieren dejar un mensaje para el equipo pueden usar esa seccion. Ejemplo: Un video o algo que crean
+el equipo necesite ver o cosas por el estilo coloquenlo ahi.
+
+PASOS DEL PROYECTO A REALIZAR
+
+escuelas Primaria y Preprimaria
+
+Registrar estudiantes y Mestros
+
+Estudiantes datos a sacar: Id, Nombre, apellido, Carnet, Grado, CUI
+
+Titulares: Id, CUI, Telefono, Nombre, apelldio, Email, Dirección (Departamento y municipio)
+
+Catedraticos: Id, CUI, Telefono, Nombre, apelldio, Email, Dirección, Grado y seccion
+
+Cursos: Grados (con esta variable se va relaciona a los catedra y alumnos)
+	horario de clases, activades, calendario de examenes.
+
+
+Implementaciones finales: Implementar dashboard, CNB
+
+
+
+NOVEDADES: por el momento nada.--}}
